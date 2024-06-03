4c55213e05576afd16e755c18d3deadb6614decf
unknown <User@DESKTOP-TLH5DPB.uocfot.local>
2024-06-03 10:30:24 +0530

    added Model and Fragment

---
+++ ToDoProject/app/src/main/java/com/hfad/todoproject/Model/ToDoModel.java
@@ -0,0 +1,27 @@
+package com.hfad.todoproject.Model;
+
+import java.util.UUID;
+
+public class ToDoModel {
+private  String Task;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    private  String Id;
+
+    public String getTask() {
+        return Task;
+    }
+
+    public void setTask(String task) {
+        Task = task;
+    }
+
+
+  }
